5ff9ca6e0bec54a2cf48cb4290ef38b2f3ebb224
shahryar saljoughi <s.saljoughi@outlook.com>
2020-05-22 19:19:55 +0430

    reached working version

--- FFX.cs
+++ FFX.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace FormatPreservignEncryption
 {
@@ -42,20 +43,60 @@ namespace FormatPreservignEncryption
 
         private Block EncryptBlock(Block originalBlock)
         {
-            // because the block size is not even, so we are having an unbalanced feistel network:
-            // left and right parts do not have the same length:
-            var (left, right) = originalBlock.Split(BlockSize / 2);
-            var result = new Block() { BlockSize = originalBlock.BlockSize};
+            // the block size is not necessarily even, hence, we are having an unbalanced Feistel network:
+
+            var (left, right, remainder) = originalBlock.SplitInHalf();
 
             for (int i = 0; i < RoundNumber; i++)
             {
-                
+                var permutedLeftPart = RoundFunction(left);
+                var temp = MergeBlockParts(permutedLeftPart, right);
+                if (i < RoundNumber - 1)
+                {
+                    right = left;
+                    left = temp;
+                }
+                else
+                {
+                    right = temp;
+                }
             }
+
+            var result = left + right;
+            if (!(remainder is null))
+            {
+                result += remainder;
+            }
+
+            return result.ToBlock();
         }
 
-        private BlockPart F(BlockPart b)
+        /// <summary>
+        /// This is equivalent to XOR operation, but customized for our requirements.
+        /// </summary>
+        /// <param name="permutedLeftPart"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        private BlockPart MergeBlockParts(BlockPart permutedLeftPart, BlockPart right)
         {
-            throw new NotImplementedException();
+            if (permutedLeftPart.BlockSize != right.BlockSize)
+                throw new Exception("operands should have the same block size to be merged");
+            var result = new BlockPart() {BlockSize = permutedLeftPart.BlockSize};
+            result.Characters = permutedLeftPart.Characters
+                .Zip(right.Characters, (x, y) => (ushort) ((x + y) % Radix))
+                .ToArray();
+            return result;
+        }
+
+        private BlockPart RoundFunction(BlockPart b)
+        {
+            var result = new BlockPart(b.BlockSize);
+            for (int i = 0; i < b.BlockSize; i++)
+            {
+                result.Characters[i] = (ushort) (Radix - b.Characters[i]);
+            }
+
+            return result;
         }
 
         private BlockPart AddBlockParts(BlockPart b1, BlockPart b2)
@@ -65,12 +106,15 @@ namespace FormatPreservignEncryption
 
         private ICollection<Block> GetBlocks(ICollection<ushort> text)
         {
-            var numberOnNeededBlocks = (int) Math.Ceiling(text.Count / (double) BlockSize);
-            var blocks = new Block[numberOnNeededBlocks];
-            for (int i = 0; i < numberOnNeededBlocks; i++)
+            var numberOfNeededBlocks = (int) Math.Ceiling(text.Count / (double) BlockSize);
+            var blocks = new Block[numberOfNeededBlocks];
+            for (int i = 0; i < numberOfNeededBlocks; i++)
             {
                 blocks[i] = new Block
-                    {Characters = text.Skip(i * BlockSize).Take(BlockSize).ToArray(), BlockSize = BlockSize};
+                {
+                    Characters = text.Skip(i * BlockSize).Take(BlockSize).PadIfNeeded(BlockSize, (ushort) 0).ToArray(),
+                    BlockSize = BlockSize
+                };
             }
 
             return blocks;
@@ -83,7 +127,7 @@ namespace FormatPreservignEncryption
 
         private string ConvertNumbersToText(IEnumerable<ushort> numbers)
         {
-            return numbers.Select(n => _numToCharMapping[n]).ToString();
+            return new string(numbers.Select(n => _numToCharMapping[n]).ToArray());
         }
     }
 
@@ -106,10 +150,54 @@ namespace FormatPreservignEncryption
             return (left, right);
         }
 
+        /// <summary>
+        /// if block size is even, remainder is null. otherwise, it will be a BlockPart holding the last character of this block.
+        /// remainder BlockPart, won't participate in ciphering. it will remain untouched in result. later we can use secret key to decide which character,
+        /// will be considered as the remainder BlockPart.
+        /// this is because, left and right parts should be of the same length for the cipher to work.
+        /// </summary>
+        /// <returns></returns>
+        public (BlockPart left, BlockPart right, BlockPart remainder) SplitInHalf()
+        {
+            BlockPart left, right, remainder;
+            if (BlockSize % 2 == 0)
+            {
+                remainder = null;
+                left = this.SubRange<BlockPart>(0, BlockSize / 2);
+                right = this.SubRange<BlockPart>(BlockSize / 2, BlockSize / 2);
+            }
+            else
+            {
+                remainder = this.SubRange<BlockPart>(BlockSize - 1, 1);
+                left = this.SubRange<BlockPart>(0, (BlockSize - 1) / 2);
+                right = this.SubRange<BlockPart>((BlockSize - 1) / 2, (BlockSize - 1) / 2);
+            }
+
+            return (left, right, remainder);
+        }
     }
 
     class BlockPart : BlockBase<BlockPart>
     {
+        public Block ToBlock() => new Block() {BlockSize = this.BlockSize, Characters = this.Characters};
+
+        public static BlockPart operator +(BlockPart b1, BlockPart b2)
+        {
+            var result = new BlockPart();
+            result.BlockSize = b1.BlockSize + b2.BlockSize;
+            result.Characters = b1.Characters.Concat(b2.Characters).ToArray();
+            return result;
+        }
+
+        public BlockPart()
+        {
+        }
+
+        public BlockPart(int blockSize)
+        {
+            BlockSize = blockSize;
+            Characters = new ushort[blockSize];
+        }
     }
 
     class BlockBase<T> : BlockBase where T : BlockBase, new()
@@ -120,11 +208,47 @@ namespace FormatPreservignEncryption
             destination.Characters = new ushort[source.Characters.Length];
             source.Characters.CopyTo(destination.Characters, 0);
         }
+
+        /// <summary>
+        /// start index is zero-based and is included in the returned Block.
+        /// </summary>
+        /// <typeparam name="U">type of output. must inherit from BlockBase</typeparam>
+        /// <param name="start"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public U SubRange<U>(int start, int? count) where U : BlockBase, new()
+        {
+            if (count is null)
+            {
+                count = Characters.Length - start;
+            }
+
+            var result = new U();
+            result.BlockSize = count.Value;
+            result.Characters = Characters.Skip(start).Take(count.Value).ToArray();
+            return result;
+        }
     }
 
     public class BlockBase
     {
         public int BlockSize;
-        public ushort[] Characters;
+        public ushort[] Characters { get; set; }
+    }
+
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> PadIfNeeded<T>(this IEnumerable<T> input, int size, T padWidth)
+        {
+            var collection = input as T[] ?? input.ToArray();
+            if (collection.Count() == size)
+            {
+                return collection;
+            }
+
+            var result = new List<T>(collection);
+            result.AddRange(Enumerable.Repeat(padWidth, size - result.Count));
+            return result;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using FormatPreservignEncryption;
 
 namespace FormatPreservingEncryption
 {
@@ -6,9 +8,22 @@ namespace FormatPreservingEncryption
     {
         static void Main(string[] args)
         {
-            var encryptor = new Numeric5digitStringEncryptor();
-            var e = encryptor.Encrypt("1");
-            Console.WriteLine(e);
+            var input = new[]
+            {
+                "10000", "10001", "10002", "10003", "10004", "10005", "10006", "10007", "10008", "10009", "10010",
+                "10011", "10012"
+            };
+            var fiveDigitCipher = new FFX(new[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'});
+            fiveDigitCipher.BlockSize = 5;
+            var result = input.Select(fiveDigitCipher.EncryptText).ToArray();
+
+            var fourDigitCipher = new FFX(new[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'});
+            fourDigitCipher.BlockSize = 4;
+            var result2 = new[] {"1000", "1001", "1002", "1003", "1111", "1112", "2222", "2223", "3456", "3457", "9980"}
+                .Select(fourDigitCipher.EncryptText).ToArray();
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+//            var ciphered = ?
         }
     }
 }
